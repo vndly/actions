1d2c735c83de07410c72a52bb4a32c89725da68b
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-11-03 21:43:31 +0100

    Improvements

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -28,9 +28,81 @@ class Buttons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
-        children: [],
+        children: [
+          Row(
+            children: [
+              const HBox(5),
+              Button(
+                name: 'Button 1',
+                url:
+                    'https://cdn.freesound.org/previews/90/90137_1430216-lq.mp3',
+                state: state,
+              ),
+              Button(
+                name: 'Button 2',
+                url:
+                    'https://cdn.freesound.org/previews/340/340955_5858296-lq.mp3',
+                state: state,
+              ),
+              const HBox(5),
+            ],
+          ),
+          Row(
+            children: [
+              const HBox(5),
+              Button(
+                name: 'Button 3',
+                url:
+                    'https://cdn.freesound.org/previews/323/323077_5514134-lq.mp3',
+                state: state,
+              ),
+              Button(
+                name: 'Button 4',
+                url:
+                    'https://cdn.freesound.org/previews/395/395822_7509543-lq.mp3',
+                state: state,
+              ),
+              const HBox(5),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  final String name;
+  final String url;
+  final MainState state;
+
+  const Button({
+    required this.name,
+    required this.url,
+    required this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        margin: const EdgeInsets.all(10),
+        height: 60,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () => state.onPlaySound(url),
+            child: Center(
+              child: Text(
+                name,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/main_state.dart
+++ lib/main_state.dart
@@ -1,5 +1,10 @@
 import 'package:dafluta/dafluta.dart';
+import 'package:just_audio/just_audio.dart';
 
 class MainState extends BaseState {
-  void playSound(String url) {}
+  Future onPlaySound(String url) async {
+    final player = AudioPlayer(); // Create a player
+    await player.setUrl(url);
+    player.play();
+  }
 }
